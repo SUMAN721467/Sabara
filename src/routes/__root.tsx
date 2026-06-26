dd0f22323673c090f7a11fafb88811a304aef29f
SUMAN721467 <152320507+SUMAN721467@users.noreply.github.com>
2026-06-26 14:47:50 +0530

    motion glitch fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,9 +4,9 @@ import {
   Link,
   createRootRouteWithContext,
   useRouter,
+  useRouterState,
   HeadContent,
   Scripts,
-  useLocation,
 } from "@tanstack/react-router";
 
 import appCss from "../styles.css?url";
@@ -161,7 +161,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
-  const location = useLocation();
+  const matches = useRouterState({ select: (s) => s.matches });
+  const currentMatch = matches[matches.length - 1];
+  const pageKey = currentMatch ? currentMatch.id : "root";
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -172,7 +174,7 @@ function RootComponent() {
               <AnnouncementBar />
               <Navbar />
               <main className="flex-1 overflow-x-hidden">
-                <div key={location.pathname} className="animate-page-enter">
+                <div key={pageKey} className="animate-page-enter">
                   <Outlet />
                 </div>
               </main>
